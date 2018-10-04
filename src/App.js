3bcba538d8e239ad2741d2ad9fa54c1d9e3e554b
joshseyda <josh.seyda@gmail.com>
2018-10-04 14:50:03 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return ( < div className = "App" >
+             <header className = "App-header" >
+            <img src = { logo }
+            className = "App-logo"
+            alt = "logo" />
+            <h1 className = "App-title" > React App v.0.0.1 </h1> </header > <p className = "App-intro" >
+            Soon to come more FN details. </p > 
+            <p>Maybe a redbull swag shop? or symphony on the potomac? maybe a place to make smart contracts with your friends for making bets?</p>
+            <Person />
+            </div> 
+        );
+    }
 }
 
 export default App;
