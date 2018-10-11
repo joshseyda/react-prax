e90c717d6ca79d32467f4d3ef163176cb6755ede
joshseyda <josh.seyda@gmail.com>
2018-10-11 17:59:47 -0400

    person component works

--- src/App.js
+++ src/App.js
@@ -5,16 +5,16 @@ import Person from './Person/Person'
 
 class App extends Component {
     render() {
-        return ( < div className = "App" >
-             <header className = "App-header" >
-            <img src = { logo }
-            className = "App-logo"
-            alt = "logo" />
-            <h1 className = "App-title" > React App v.0.0.1 </h1> </header > <p className = "App-intro" >
-            Soon to come more FN details. </p > 
+        return ( 
+        <div className = "App">
+             <header className = "App-header">
+            <img src={ logo }className = "App-logo" alt = "logo"/>
             <p>Maybe a redbull swag shop? or symphony on the potomac? maybe a place to make smart contracts with your friends for making bets?</p>
+            </header>
             <Person />
-            </div> 
+            <Person />
+            <Person />
+        </div> 
         );
     }
 }
