2aa20b0249a2b7d0ae2232993946a893760ac11a
joshseyda <josh.seyda@gmail.com>
2018-10-12 13:47:19 -0400

    map() at its finest

--- src/App.js
+++ src/App.js
@@ -51,22 +51,11 @@ class App extends Component {
     
     let persons = null;
         if (this.state.showPersons) {
-            persons = (<div>
-                <Person 
-                    name={this.state.persons[0].name} 
-                    age={this.state.persons[0].age}
-                    click={this.switchNameHandler.bind(this, "JoRsH")}/>
-                <Person 
-                    name={this.state.persons[1].name} 
-                    age={this.state.persons[1].age}
-                    click={this.switchNameHandler.bind(this, "JISH")}
-                    changed={this.nameChangedHandler}>
-                    And I am an alter ego!
-                </Person>
-                <Person 
-                    name={this.state.persons[2].name} 
-                    age={this.state.persons[2].age}
-                    click={this.switchNameHandler.bind(this, "yeshua")}/>
+            persons = (
+            <div>
+                {this.state.persons.map(person => {
+                    return <Person name={person.name} age={person.age}/>
+                })}
             </div>);
         }
 
