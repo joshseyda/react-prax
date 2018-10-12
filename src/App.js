6ec853b4a0fb0142db9952c6290a773c261cf914
joshseyda <josh.seyda@gmail.com>
2018-10-11 22:31:53 -0400

    inefficient albeit useful alternative to bind in chinging state

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends Component {
     render() {
         return ( 
         <div className = "App">
-            <button onClick={this.switchNameHandler.bind(this, "Joshua")}>Switch Name!</button>
+            <button onClick={() => this.switchNameHandler("Joshua!!!")}>Switch Name!</button>
             <Person 
                 name={this.state.persons[0].name} 
                 age={this.state.persons[0].age}
