5741576aee2a76de6f9d01210de9dc00b1b522ab
joshseyda <josh.seyda@gmail.com>
2018-11-08 12:24:47 -0500

    Test of Lifecycle Hooks

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -2,7 +2,20 @@ import React, {Component} from 'react';
 import Person from  './Person/Person';
 
 class Persons extends Component  {
+    constructor(props){
+        super(props);
+        console.log('[Persons.js] constructor', props);
+    }
+
+    componentWillMount(){
+        console.log('[Persons.js] componentWillMount');
+    }
+
+    componentDidMount(){
+        console.log('[Persons.js] componentDidMount');
+    }
     render () {
+        console.log('[Persons.js] Render');
         return this.props.persons.map((person, index) => {
         return <Person 
             key={person.id}
