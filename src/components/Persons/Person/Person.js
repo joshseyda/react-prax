9ff394179b21e9c750a3855ac35ee9ba9d9540cd
joshseyda <josh.seyda@gmail.com>
2019-01-29 11:56:28 -0500

    using references in stateful components. ie focus

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -5,13 +5,21 @@ import Aux from '../../../hoc/Aux';
 import PropTypes, { checkPropTypes } from 'prop-types';
 
 class Person extends Component {
+  componentDidMount () {
+    if(this.props.position == 0){this.inputElement.focus();}
+  }
   render () {
     console.log('[Person.js] Render');
     return (
       <Aux>
         <p onClick={this.props.click}> I'm {this.props.name} and I am {this.props.age} years old! </p>
         <p>{this.props.children}</p>
-        <input type="text" onChange={this.props.changed} value={this.props.name}/>
+        <input 
+          ref={(inp) => {this.inputElement = inp}}
+          type="text" 
+          onChange={this.props.changed} 
+          value={this.props.name}
+        />
       </Aux>
     )
   }
